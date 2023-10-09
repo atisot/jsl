e5e3939f1480627435d33a4d6bdc6bdde5b1d453
bagomot <perezzloy@gmail.com>
2023-10-10 00:36:03 +0300

    update manager

--- source/jsonstyling/manager.d
+++ source/jsonstyling/manager.d
@@ -2,9 +2,10 @@ module jsonstyling.manager;
 
 import std.file;
 import std.json;
-
+import std.typecons;
 
 import jsonstyling;
+import etc.c.curl;
 
 alias JsonStyling = JsonStylingManager.instance;
 
@@ -12,6 +13,7 @@ class JsonStylingManager {
     private {
         __gshared JsonStylingManager _instance;
         Theme[string] _themes;
+        string _currentThemeId;
     }
 
     protected this() {
@@ -33,22 +35,39 @@ class JsonStylingManager {
         return _instance;
     }
 
+    void currentTheme(string themeId)
+    {
+        if (themeId !in _themes)
+        {
+            throw new Exception("Theme not found: " ~ themeId);
+        }
+        _currentThemeId = themeId;
+    }
+
+    // Получение текущей темы
+    Theme currentTheme()
+    {
+        if (_currentThemeId)
+        {
+            return _themes[_currentThemeId];
+        }
+        else
+        {
+            throw new Exception("No current theme set.");
+        }
+    }
+
     // Загрузка темы из файла
     void loadThemeFromFile(string filePath)
     {
         try
         {
             string content = readText(filePath);
-            JSONValue json = parseJSON(content);
-            Theme theme = Theme.parse(json);
-            _themes[theme.id] = theme;
+            loadThemeFromMemory(content);
         }
         catch (FileException e)
         {
             throw new Exception("Error read theme file: " ~ e.msg);
-        }
-        catch (JSONException e) {
-            throw new Exception("Error parse theme: " ~ e.msg);
         }
         catch (Exception e)
         {
@@ -64,6 +83,10 @@ class JsonStylingManager {
             JSONValue json = parseJSON(content);
             Theme theme = Theme.parse(json);
             _themes[theme.id] = theme;
+            if (_themes.length == 1)
+            {
+                currentTheme(theme.id);
+            }
         }
         catch (JSONException e)
         {
@@ -74,26 +97,6 @@ class JsonStylingManager {
             throw new Exception("Error: " ~ e.msg);
         }
     }
-
-    // Загрузка темы из ресурсов (предполагается, что у вас есть функция loadResource)
-    // void loadThemeFromResource(string resourceId)
-    // {
-    //     try
-    //     {
-    //         string content = loadResource(resourceId);
-    //         JSONValue json = parseJSON(content);
-    //         Theme theme = Theme.parse(json);
-    //         _themes[theme.id] = theme;
-    //     }
-    //     catch (JSONException e)
-    //     {
-    //         throw new Exception("Error parse theme: " ~ e.msg);
-    //     }
-    //     catch (Exception e)
-    //     {
-    //         throw new Exception("Error: " ~ e.msg);
-    //     }
-    // }
 
     // Получение списка ID всех тем
     string[] themeIds()
@@ -127,5 +130,63 @@ class JsonStylingManager {
     void clearThemes()
     {
         _themes.clear();
+    }
+
+    T property(T)(string styleId, string propName)
+    {
+        auto prop = findPropertyInTheme!T(currentTheme, styleId, propName);
+        if(prop.isNull)
+        {
+            throw new Exception("Property " ~ propName ~ " not found");
+        }
+        return prop.get;
+    }
+
+    private Nullable!T findPropertyInTheme(T)(Theme theme, string styleId, string propName)
+    {
+        // Попробовать найти стиль в текущей теме
+        auto style = theme.style(styleId);
+        if (!style.isNull)
+        {
+            // Попробовать найти свойство в найденном стиле
+            auto prop = style.get.property!T(propName);
+            if (!prop.isNull)
+            {
+                return prop;
+            }
+            // Если свойство не найдено, ищем в родительском стиле
+        else if (style.get.parentId !is null)
+            {
+                return findPropertyInTheme!T(theme, style.get.parentId, propName);
+            }
+        }
+
+        // Если стиль или свойство не найдено в текущей теме, ищем в родительской теме
+        if (theme.parentId !is null)
+        {
+            Theme parentTheme = this.theme(theme.parentId);
+            return findPropertyInTheme!T(parentTheme, styleId, propName);
+        }
+
+        // Если стиль или свойство не найдено и нет родительской темы
+        return Nullable!T.init;
+    }
+
+    Nullable!Style findStyleInTheme(Theme theme, string styleId)
+    {
+        auto style = theme.style(styleId);
+        if (!style.isNull)
+        {
+            return style;
+        }
+        else if (theme.parentId !is null)
+        {
+            Theme parentTheme = this.theme(theme.parentId);
+            return findStyleInTheme(parentTheme, styleId);
+        }
+        else
+        {
+            return Nullable!Style.init;
+        }
     }
 }
